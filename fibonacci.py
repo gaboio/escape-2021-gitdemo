6d2f714f1392c8e5fe88a7895de34020122f06af
Gabriel Oio <gabrieloio@unc.edu.ar>
2021-06-08 16:26:24 -0300

    Add fibonacci generator

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,16 @@
+from functools import cache
+
+@cache
 def fibonacci(n):
     if n == 0:
         return 0
     if n==1:
         return 1
     return fibonacci(n-1)+fibonacci(n-2)
+
+def fib_gen(n):
+    for i in range (n):
+        yield fibonacci(i)
 
 if __name__ == '__main__':
     print(fibonacci(20))
